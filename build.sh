d855993d42a6cfbe92b079ca42d58745c0259bc2
Aymen Furter <aymen.furter@gmail.com>
2017-05-30 19:27:06 +0200

    Removing local shell script

--- build.sh
+++
@@ -1,5 +0,0 @@
-ng build
-rsync -a ./dist/ /media/mika/fshome/dev/git/imop/cxf-rest-service/src/main/webapp/
-cd /media/mika/fshome/dev/git/imop
-sh /media/mika/fshome/dev/git/imop/deploy.sh
-cd ~/main/dev/git/imopsng/enhance
